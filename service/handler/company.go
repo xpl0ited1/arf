4b7d934a0f7fca8a8a8f54a92f0ba118adde05c0
asd asd <b.muhlhausera@gmail.com>
2022-06-01 01:49:01 -0400

    Updated company handler

--- service/handler/company.go
+++ service/handler/company.go
@@ -1,23 +1,53 @@
 package handler
 
-import "net/http"
+import (
+	"activeReconBot/dao"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func CreateCompany(w http.ResponseWriter, r *http.Request) {
-	RespondJSON(w, http.StatusOK, "Testing Api")
+	result, err := dao.CreateCompany(r)
+	if err != nil {
+		RespondError(w, http.StatusUnprocessableEntity, err.Error())
+	}
+	RespondJSON(w, http.StatusCreated, result)
 }
 
 func UpdateCompany(w http.ResponseWriter, r *http.Request) {
-	RespondJSON(w, http.StatusOK, "Testing Api")
+	vars := mux.Vars(r)
+	companyID := vars["company_id"]
+	result, err := dao.UpdateCompany(companyID, r)
+	if err != nil {
+		RespondError(w, http.StatusUnprocessableEntity, err.Error())
+	}
+	RespondJSON(w, http.StatusOK, result)
 }
 
 func DeleteCompany(w http.ResponseWriter, r *http.Request) {
-	RespondJSON(w, http.StatusOK, "Testing Api")
+	vars := mux.Vars(r)
+	companyID := vars["company_id"]
+	result, err := dao.DeleteCompany(companyID)
+	if err != nil {
+		RespondError(w, http.StatusUnprocessableEntity, err.Error())
+	}
+	RespondJSON(w, http.StatusOK, result)
 }
 
 func GetCompanies(w http.ResponseWriter, r *http.Request) {
-	RespondJSON(w, http.StatusOK, "Testing Api")
+	result, err := dao.GetCompanies()
+	if err != nil {
+		RespondError(w, http.StatusInternalServerError, err.Error())
+	}
+	RespondJSON(w, http.StatusOK, result)
 }
 
 func GetCompany(w http.ResponseWriter, r *http.Request) {
-	RespondJSON(w, http.StatusOK, "Testing Api")
+	vars := mux.Vars(r)
+	companyID := vars["company_id"]
+	result, err := dao.GetCompany(companyID)
+	if err != nil {
+		RespondError(w, http.StatusUnprocessableEntity, err.Error())
+	}
+	RespondJSON(w, http.StatusOK, result)
 }
